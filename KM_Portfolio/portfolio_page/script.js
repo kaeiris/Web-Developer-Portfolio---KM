d142a079e4d69b8a3eed717f39abb63735e19239
kaeiris <36251385+kaeiris@users.noreply.github.com>
2025-04-22 18:40:30 -0500

    Delete KM_Portfolio directory

--- KM_Portfolio/portfolio_page/script.js
+++
@@ -1,8 +0,0 @@
-function dropdownMenu(){
-    let dropdown = document.querySelector('.dropdown-menu-wrapper');
-
-    dropdown.addEventListener('click', ()=>{
-        document.querySelector('.nav-button').classList.toggle('hide');
-
-    });
-}
